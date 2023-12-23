7fab19256dbf805a75693c0e1e4e6f77a01e0609
Huynh Minh Triet <100458317+Anshler@users.noreply.github.com>
2023-12-23 11:55:01 +0700

    Update install.py

--- install.py
+++ install.py
@@ -4,9 +4,9 @@ import launch
 
 # if not launch.is_installed("aitextgen"):
 #     launch.run_pip("install aitextgen==0.6.0", "requirements for MagicPrompt")
-if not launch.is_installed("transformers==4.35.2"):
-    launch.run_pip("install accelerate==0.20.3", "requirements for Image Caption")
-    launch.run_pip("install transformers==4.35.2", "requirements for Image Caption")
+if not launch.is_installed("transformers>=4.35.2"):
+    launch.run_pip("install accelerate>=0.20.3", "requirements for Image Caption")
+    launch.run_pip("install transformers>=4.35.2", "requirements for Image Caption")
 
 if not launch.is_installed("open_clip_torch"):
     launch.run_pip("install open_clip_torch", "requirements for Image Caption")
